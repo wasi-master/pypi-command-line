b113d85885facd2145060d2baaea71b91e59ebc4
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-10-22 21:41:53 +0600

    Improve the publish script

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,11 @@
-Remove-Item dist/*
 $NewVersion = Read-Host -Prompt 'Enter the new version'
 $Comment = Read-Host -Prompt 'Enter a comment for the new release'
+Write-Host ""
+Write-Host "Removing previous distributions" -ForegroundColor Green
+Remove-Item dist/*
+Write-Host "Changing version number" -ForegroundColor Green
 (Get-Content .\pypi_cli\__init__.py) -replace '__version__\s+=\s+"(.+)"', ('__version__ = "{0}"' -f $NewVersion)  | Set-Content .\pypi_cli\__init__.py
+Write-Host "Buliding Package" -ForegroundColor Green
 python -m build
+Write-Host "Uploading Package" -ForegroundColor Green
 twine upload dist/* -p $env:TWINE_PASSWORD -u $env:TWINE_USERNAME -c $Comment -r pypi
