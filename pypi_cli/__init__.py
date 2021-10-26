927d358238a7affe793ab4373f32d28da5654fe5
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-10-26 11:10:37 +0600

    Bump version to 1.4.0

--- pypi_cli/__init__.py
+++ pypi_cli/__init__.py
@@ -3,6 +3,6 @@
 __name__ = "pypi-command-line"
 __title__ = __name__
 __license__ = "MIT"
-__version__ = "1.3.7"
+__version__ = "1.4.0"
 __author__ = "Arian Mollik Wasi"
 __github__ = "https://github.com/wasi-master/pypi-cli"
