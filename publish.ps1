cc4631991c6fc7837ea1b454a941dbfc6957e212
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-10-26 11:31:19 +0600

    Use variables

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,5 @@
-$CurrentVersion = (Select-String -Path .\pypi_cli\__init__.py -Pattern '__version__\s+=\s+"(.+)"').Matches[0].Groups[1].Value
+$InitFilePath = ".\pypi_cli\__init__.py"
+$CurrentVersion = (Select-String -Path $InitFilePath -Pattern '__version__\s+=\s+"(.+)"').Matches[0].Groups[1].Value
 Write-Host "Current Version: $CurrentVersion" -ForegroundColor Blue
 $NewVersion = Read-Host -Prompt 'Enter the new version'
 $Comment = Read-Host -Prompt 'Enter a comment for the new release'
@@ -6,7 +7,7 @@ Write-Host ""
 Write-Host "Removing previous distributions" -ForegroundColor Green
 Remove-Item dist/*
 Write-Host "Changing version number" -ForegroundColor Green
-(Get-Content .\pypi_cli\__init__.py) -Replace '__version__\s+=\s+"(.+)"', ('__version__ = "{0}"' -f $NewVersion)  | Set-Content .\pypi_cli\__init__.py
+(Get-Content $InitFilePath) -Replace '__version__\s+=\s+"(.+)"', ('__version__ = "{0}"' -f $NewVersion)  | Set-Content $InitFilePath
 Write-Host "Buliding Package" -ForegroundColor Green
 python -m build
 Write-Host "Uploading Package" -ForegroundColor Green
