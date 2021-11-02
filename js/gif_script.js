016a7f28056448688c0dc9cf30e35f74318854c3
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-11-02 18:29:48 +0600

    Deployed ecdb5f0 with MkDocs version: 1.2.2

---
+++ js/gif_script.js
@@ -0,0 +1,24 @@
+console.log("GIF reloading script loaded")
+
+function restartGif (gifElement) {
+  const gifSrc = gifElement.src
+  gifElement.src = gifSrc
+}
+
+window.onload = function () {
+  const images = document.getElementsByTagName('img')
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i]
+    if (!image.src.endsWith('.gif')) {
+      continue
+    }
+    image.onclick = function () { restartGif(image) }
+    if (image.title) {
+      image.title += " (Click to restart)"
+    } else if (image.alt) {
+      image.title = image.alt + " (Click to restart)"
+    } else {
+      image.title = "Click to restart"
+    }
+  }
+}
